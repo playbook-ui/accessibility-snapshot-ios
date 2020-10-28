2ccae7d7115d0d0843a09f49bba8b80e4c50c3d7
Jiayi Zhou <z-jiayi@mercari.com>
2020-10-28 15:21:53 -0400

    Add Apple's Accessibility project as example

--- Example/SampleApp/Scenarios.swift
+++ Example/SampleApp/Scenarios.swift
@@ -3,9 +3,29 @@ import SwiftUI
 
 struct AllScenarios: ScenarioProvider {
     static func addScenarios(into playbook: Playbook) {
-        playbook.addScenarios(of: "Content") {
-            Scenario("ContentView", layout: .fill) {
-                ContentView()
+        playbook.addScenarios(of: "Accessibility") {
+            Scenario("Standard Controls", layout: .fill) {
+                StandardControlExample()
+            }
+            
+            Scenario("Images", layout: .fill) {
+                ImageExample()
+            }
+            
+            Scenario("Text", layout: .fill) {
+                TextExample()
+            }
+            
+            Scenario("Containers", layout: .fill) {
+                ContainerExample()
+            }
+            
+            Scenario("Actions", layout: .fill) {
+                ActionExample()
+            }
+            
+            Scenario("ViewRepresentable", layout: .fill) {
+                RepresentableExample()
             }
         }
     }
--- Tools/Package.swift
+++ Tools/Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "Tools",
     dependencies: [
         .package(url: "https://github.com/ra1028/swift-mod.git", .exact("0.0.4")),
-        .package(url: "https://github.com/apple/swift-format.git", .branch("swift-5.1-branch")),
+        .package(url: "https://github.com/apple/swift-format.git", .branch("swift-5.3-branch")),
         .package(url: "https://github.com/yonaskolb/XcodeGen.git", .exact("2.18.0")),
         .package(url: "https://github.com/SwiftDocOrg/swift-doc.git", .branch("1.0.0-beta.5")),
     ],
